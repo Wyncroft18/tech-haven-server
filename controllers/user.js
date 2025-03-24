0ed311e383bfaabbc33389f2fe2ad78d5c0a9039
Wyncroft18 <kapulong18@gmail.com>
2025-03-24 20:29:02 +0800

    Add bcrypt for password and clear form on submit

--- controllers/user.js
+++ controllers/user.js
@@ -1,23 +1,30 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 module.exports.registerUser = async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        const existingUser = await User.exists({email})
-        if (existingUser){
-            return res.status(400).json({error: "Email is already registered."})
+        const existingUser = await User.exists({ email });
+        if (existingUser) {
+            return res
+                .status(409)
+                .json({ error: "Email is already registered." });
         }
-        
-        const newUser = new User({ email, password });
-        const result = await newUser.save()
+
+        const newUser = new User({
+            email: email,
+            password: bcrypt.hashSync(password, 10),
+        });
+
+        const result = await newUser.save();
 
         res.status(201).json({
             message: "User registered successfully.",
-            user: result
-        })
+            user: result,
+        });
     } catch (error) {
-        console.error("Registration error:", error)
+        console.error("Registration error:", error);
         res.status(500).json({ error: "Error in registering user." });
     }
 };
