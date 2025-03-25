1c84b0e18af3393375f7c2a0d595a5cead102446
Wyncroft18 <kapulong18@gmail.com>
2025-03-25 20:52:47 +0800

    Add image url to product model

--- models/User.js
+++ models/User.js
@@ -13,6 +13,10 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
+    imgUrl: {
+        type: String,
+        required: [true, "Image is required."],
+    },
 });
 
 module.exports = mongoose.model("User", userSchema);
