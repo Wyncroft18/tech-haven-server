eaadf8fc3ee1ea47606fe2204eb96aeaba1dca40
Wyncroft18 <kapulong18@gmail.com>
2025-07-01 05:10:43 +0800

    Refactor product controller

--- controllers/product.js
+++ controllers/product.js
@@ -1,45 +1,48 @@
+// imports
 const Product = require("../models/Product");
 
-module.exports.getAllProducts = async (req, res) => {
+exports.getAllProducts = async (req, res) => {
     try {
         const products = await Product.find();
-        if (products.length > 0) {
-            return res.status(200).json({ products });
-        } else {
+
+        if (!products) {
             return res.status(200).json({ message: "No products found." });
         }
+
+        return res.status(200).json({ products });
     } catch (error) {
         console.error("Error in finding all products:", error);
         res.status(500).json({ error: "Error in finding products." });
     }
 };
 
-module.exports.addProduct = async (req, res) => {
+exports.addProduct = async (req, res) => {
     try {
         const { name, description, price, imgUrl, alt } = req.body;
 
         const existingProduct = await Product.findOne({ name });
+
         if (existingProduct) {
             return res.status(409).json({ error: "Product already exists." });
-        } else {
-            let newProduct = new Product({
-                name,
-                description,
-                price,
-                imgUrl,
-                alt,
-            });
-
-            const result = await newProduct.save();
-            res.status(201).json(result);
         }
+
+        const newProduct = new Product({
+            name,
+            description,
+            price,
+            imgUrl,
+            alt,
+        });
+
+        const result = await newProduct.save();
+        res.status(201).json(result);
     } catch (error) {
         console.error("Error in adding product:", error);
         res.status(500).json({ error: "Error in adding product." });
     }
 };
 
-module.exports.getSingleProduct = async (req, res) => {
+exports.getSingleProduct = async (req, res) => {
     try {
         const product = await Product.findById(req.params.productId);
 
